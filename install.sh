51b81fa5eda73ac1ecc3f2f05a49abdaf0b21c18
d-eder <d.eder@outlook.at>
2024-02-12 10:42:23 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install snapd
+sudo snap install core
+sudo snap install ngrok
+sudo /snap/ngrok/current/ngrok config add-authtoken 2c7v3Du24Db8Ku9uYsIXMb4kAqk_FxDXg915xhg7tCJST4Di
+
+sudo snap install openjdk
+sudo reboot
